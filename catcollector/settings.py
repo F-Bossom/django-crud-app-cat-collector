11ce09ffd07648a89335f3e84c57724e4aadc04d
fbossom <felicia.bossom@yahoo.com>
2026-02-23 17:06:47 -0500

    continued with lecture materials

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'catcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catcollector',
+        # Some may have connection issues for username and password
+        "username": "postgres",
+        "password": "2324"
     }
 }
 
